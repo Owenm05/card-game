2173951c52314ac729f923400a9c37509f4ae7d5
Hun <43863985+Owenm05@users.noreply.github.com>
2022-12-08 09:57:50 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -25,19 +25,51 @@ def shop(gold):
       print("we have these cards:")
       for x in cards:
           print(x)
+import random
+class Game:
+  def __init__(self,enemyHp,level,gold):
+    self.enemyHp=enemyHp
+    self.level=level
+    self.gold=gold
+if __name__ == "__main__":
+    game= Game(50,random.randint(1,3),100)
+attack = {
+  "damage": 5,
+  "cost": 1,
+  "flavor": "you swing your sword"
+}
+def shop(gold):
+    global game
+    print("welcome to my shop would you like to buy anything")
+    global inventory
+    inventory=["attack","defend"]
+    cards=[]
+    print("your level is :",game.level)
+    if game.level>=1:
+        cards=["attack","defend","heal"]
+    if game.level>=2:
+        cards.append("curse")
+        cards.append("sacrifice")
+    if game.level==3:
+        cards.append("smite")
+    answer = input()
+    if answer == "yes":
+      print("we have these cards:")
+      for x in cards:
+          print(x)
       print("what would you like to buy",cards)
       gear = input()
       if gear in cards:
           gear_id = cards.index(gear)
-          if gold >= 10:
+          if game.gold >= 10:
               print("you have purchased an", cards[gear_id], "card")
-              gold -= 10
-              print("you now have", + gold,"gold")
+              game.gold -= 10
+              print("you now have", + game.gold,"gold")
               inventory.append(gear)
-              return gold
-          elif gold<10:
+              return game.gold
+          elif game.gold<10:
               print("you dont have enough gold")
-              print("you now have", + gold)
+              print("you now have", + game.gold)
              
       else:
            print("no such card")
@@ -58,9 +90,10 @@ def combat():
       damage(attack["damage"])
       
 def damage(damage):
-  enemyHp=100
-  enemyHp-=damage
-  print(enemyHp)
+  global game
+  print(game.enemyHp)
+  game.enemyHp -= damage
+  print(game.enemyHp)
   combat()
 shop(100)
 restart=input("continue?")
@@ -71,3 +104,4 @@ if restart=="yes":
     shop(gold)
 if restart=="no":
     combat()
+
